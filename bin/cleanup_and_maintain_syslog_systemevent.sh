b443b07efc4925f2c1c7315293df2453d3df3d71
stevleibelt <stev_leibelt@freiberg.de>
2021-04-20 17:59:41 +0200

    added missing iterator

--- bin/cleanup_and_maintain_syslog_systemevent.sh
+++ bin/cleanup_and_maintain_syslog_systemevent.sh
@@ -38,6 +38,7 @@ function cleanup_and_maintain_syslog_systemevent ()
     while [[ ${CURRENT_RUN_ITERATOR} -lt ${NUMBER_OF_RUNS} ]];
     do
         mysql -u ${DATABASE_USER_NAME} -p${DATABASE_USER_PASSWORD} -e "DELETE FROM ${DATABASE_TABLE} WHERE ${DATABASE_TABLE}.DeviceReportedTime < date_add(current_date, interval - ${DAYS_TO_KEEP_IN_THE_PAST} day) LIMIT ${NUMBER_OF_ENTRIES_TO_DELETE_PER_RUN}" ${DATABASE_NAME}
+        ((++CURRENT_RUN_ITERATOR))
     done
     #eo: cleanup
 
